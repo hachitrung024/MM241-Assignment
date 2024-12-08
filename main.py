828174daf5f1182cfa882164c2aa93661d6f4355
Trung Ha <hachitrung024@gmail.com>
2024-12-08 18:07:40 +0700

    update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import gym_cutting_stock
 import gymnasium as gym
 from policy import GreedyPolicy, RandomPolicy
 from student_submissions.s2210xxx.policy2210xxx import Policy2210xxx
-
+import pygame
+pygame.display.set_mode((800,800))
 
 # Create the environment
 env = gym.make(
@@ -16,7 +17,7 @@ if __name__ == "__main__":
     observation, info = env.reset(seed=42)
 
     # Test GreedyPolicy
-    gd_policy = Policy2210xxx()
+    gd_policy = Policy2210xxx(policy_id=1)
     ep = 0
     while ep < NUM_EPISODES:
         action = gd_policy.get_action(observation, info)
--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -5,6 +5,7 @@ class Policy2210xxx(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
         self.policy_id = policy_id
+        self.stock_start_idx = 0
         # Student code here
         if policy_id == 1:
             self.corner_points = {0: [(0,0)]}
@@ -14,36 +15,39 @@ class Policy2210xxx(Policy):
     def get_action(self, observation, info):
         # Student code here
         if self.policy_id == 1:
-            list_prods = sorted(observation["products"], key=lambda prod: prod["size"][0], reverse=True)
             if not info["filled_ratio"]:
                 self.corner_points = {0: [(0,0)]}
+                self.stock_start_idx = 0
             # Find first prod["quantity"] > 0
-            prod = next((prod for prod in list_prods if prod["quantity"] > 0), None)
-            if not prod:
-                return {"stock_idx": -1, "size": [0, 0], "position": (None, None)}
-            
-            prod_size = prod["size"]
-            prod_w, prod_h = prod_size
-            for stock_idx, stock in enumerate(observation["stocks"]):
-                stock_w, stock_h = self._get_stock_size_(stock)
-                if stock_idx not in self.corner_points:
-                    self.corner_points[stock_idx] = [(0, 0)]
-                for x, y in self.corner_points[stock_idx]:
-                    if (self._can_place_(observation["stocks"][stock_idx], (x, y), (prod_w, prod_h)) and
-                        x + prod_w <= stock_w and y + prod_h <= stock_h):
-                        
-                        action = {
-                            "stock_idx": stock_idx,
-                            "size": (prod_w, prod_h),
-                            "position": (x, y),
-                        }
-                        # Cập nhật điểm góc
-                        self.corner_points[stock_idx].append((x + prod_w, y))
-                        self.corner_points[stock_idx].append((x, y + prod_h))
-                        self.corner_points[stock_idx].remove((x, y))
+            list_prods = sorted(observation["products"], key=lambda prod: prod["size"][0], reverse=True)
+            stocks = observation["stocks"]
+            for stock_idx in range (self.stock_start_idx, len(stocks)):
+                stock = stocks[stock_idx]
+                for prod in list_prods:  
+                    if prod["quantity"] <= 0:
+                        continue        
+                    prod_size = prod["size"]
+                    prod_w, prod_h = prod_size
+                    stock_w, stock_h = self._get_stock_size_(stock)
+                    if stock_idx not in self.corner_points:
+                        self.corner_points[stock_idx] = [(0, 0)]
+                    for x, y in self.corner_points[stock_idx]:
+                        if (self._can_place_(observation["stocks"][stock_idx], (x, y), (prod_w, prod_h)) and
+                            x + prod_w <= stock_w and y + prod_h <= stock_h):
+                            
+                            action = {
+                                "stock_idx": stock_idx,
+                                "size": (prod_w, prod_h),
+                                "position": (x, y),
+                            }
+                            # Cập nhật điểm góc
+                            self.corner_points[stock_idx].append((x + prod_w, y))
+                            self.corner_points[stock_idx].append((x, y + prod_h))
+                            self.corner_points[stock_idx].remove((x, y))
 
-                        # Thêm tấm vào used_stocks
-                        return action
+                            # Thêm tấm vào used_stocks
+                            return action
+                self.stock_start_idx += 1
 
         pass
 
